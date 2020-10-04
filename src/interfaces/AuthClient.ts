6d6ee66aafee0d3ca1048e45f483d2cc1e8bb617
Chris Were <chris@mozzler.com.au>
2020-10-04 20:43:23 +1030

    Implement QR code generation as per the spec

---
+++ src/interfaces/AuthClient.ts
@@ -0,0 +1,12 @@
+
+
+export interface AuthClientConfig {
+    serverUri: string,
+    baseUri: string,
+    canvasId: string
+}
+
+export interface AuthResponse {
+    type: string,
+    message: any
+}
