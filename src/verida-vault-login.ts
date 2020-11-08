072e7b8b3245fbd31dcc76d72359c4b7813da19f
Chris Were <chris@mozzler.com.au>
2020-11-08 20:44:32 +1030

    Update styling

--- src/verida-vault-login.ts
+++ src/verida-vault-login.ts
@@ -52,7 +52,7 @@ export default function() {
         top: 0;
         width: 100%;
         height: 100%;
-        overflow: auto;
+        overflow: hidden;
         background-color: rgba(0,0,0,0.4);
       }
 
