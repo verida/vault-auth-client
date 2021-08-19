a7676853153bf1462d3e1009505792323bf73a32
Chris <chris@verida.io>
2021-08-19 21:35:48 +0930

    Make the logo URL optional

--- src/interfaces/AuthClient.ts
+++ src/interfaces/AuthClient.ts
@@ -6,7 +6,7 @@ export interface AuthClientConfig {
     loginUri: string,       // Login URI (page where the user will be sent to login using the app; ie: vault.verida.io)
     canvasId: string        // DOM id where the QR code canvas will be injected
     schemeUri: string,
-    logoUrl: string,
+    logoUrl?: string,
     deeplinkId: string,
     request: object,        // Authorization request object that matches https://schemas.verida.io/auth/request/schema.json
     callback(response: AuthResponse): void        // callback function (called when auth response received)
